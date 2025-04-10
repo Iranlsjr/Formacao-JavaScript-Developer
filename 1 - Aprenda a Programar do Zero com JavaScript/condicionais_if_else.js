7181e21a48224402c8644e0b6a9a157a56378544
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-09 23:30:47 -0300

    Condicionais if else

---
+++ 1 - Aprenda a Programar do Zero com JavaScript/condicionais_if_else.js
@@ -0,0 +1,13 @@
+
+const numero = 3;
+
+const numeroPar = (numero % 2) === 0;
+console.log(numeroPar)
+
+if (numeroPar){
+    console.log('O número é par!');
+}
+else {
+    console.log('O número é impar!');
+}
+
