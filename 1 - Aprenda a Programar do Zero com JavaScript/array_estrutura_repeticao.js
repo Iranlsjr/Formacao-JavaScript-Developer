cd8c1ad139de7419d8f21a90bd934d7f41c313a6
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-12 16:13:02 -0300

    Array - Estrutura de repetição

---
+++ 1 - Aprenda a Programar do Zero com JavaScript/array_estrutura_repeticao.js
@@ -0,0 +1,22 @@
+// const notas = [];
+
+// notas.push(5);
+// notas.push(7);
+// notas.push(5);
+// notas.push(2);
+// notas.push(5);
+// notas.push(8);
+
+// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+// console.log(soma /5);
+
+//Repetição
+
+const nome = 'Iran Lourenço da Silva Junior'
+
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
+    
+}
