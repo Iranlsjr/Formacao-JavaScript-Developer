d42f03f0ab6e0592dc40de5183fe34c93d9a71d1
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-12 14:27:07 -0300

    Arrays - Removendo e adicionando itens na lista

---
+++ 1 - Aprenda a Programar do Zero com JavaScript/arrays_removendo_itens.js
@@ -0,0 +1,16 @@
+
+
+const aluno = ['João', 'Iran', 'Marina'];
+
+//Adicionando itens
+aluno.push('Renan')
+aluno[4] = 'Vinícius'
+
+//Removendo ultimo item da lista
+console.log(aluno.pop())
+
+// Removendo primeiro item da lista
+console.log(aluno.shift())
+
+//Imprimindo
+console.log(aluno);
