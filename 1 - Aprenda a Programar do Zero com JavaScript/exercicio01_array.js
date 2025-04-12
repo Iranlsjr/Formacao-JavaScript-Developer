a47de7fa3c39b9109784013f893544bb73920136
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-12 16:24:45 -0300

    Exercicio 01 Array e For

--- 1 - Aprenda a Programar do Zero com JavaScript/exercicio01_array.js
+++ 1 - Aprenda a Programar do Zero com JavaScript/exercicio01_array.js
@@ -1,13 +1,20 @@
-// const notas = [];
+// Arrays - Lista
+const notas = [];
 
-// notas.push(5);
-// notas.push(7);
-// notas.push(5);
-// notas.push(2);
-// notas.push(5);
-// notas.push(8);
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
 
-// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
-// console.log(soma /5);
+let soma = 0;
 
 //Repetição
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma  + nota ;
+}
+
+const media = soma / notas.length;
+console.log(media.toFixed(2));
