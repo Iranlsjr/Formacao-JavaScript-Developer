91da91c2bcec3e58e6f424370c775908beb6a477
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-14 16:36:32 -0300

    Desafio para calcular média do aluno itulizando importação de arquivo

---
+++ 1 - Aprenda a Programar do Zero com JavaScript/importacao/desafio-media.js
@@ -0,0 +1,22 @@
+// Faça um programa que receba a média de um aluno.
+// caso a média seja < 5 imprima "Reprovado"
+// caso a média seja >= 5 e < 7 imprima "Recuperação"
+// caso a média seja >= 7 imprima "Aprovado"
+
+//     Exemplo:
+//      Entrada:
+//          5.5
+//      Saída:
+//          Recuperação.
+
+const { gets, print } = require("./funcoes-auxiliares-media");
+
+const media = gets();
+
+if (media < 5) {
+  print("Reprovado");
+} else if (media >= 5 && media < 7) {
+  print("Recuperação");
+} else {
+  print("Aprovado");
+}
