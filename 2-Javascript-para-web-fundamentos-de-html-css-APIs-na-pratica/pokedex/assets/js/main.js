a7350a6e7dd12cf9beba631ea374e504a081258d
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-17 16:01:24 -0300

    Manipulando o resultado da requisição atravez de  uma promise e  aplicando metodo de Arrow Function

--- 2-Javascript-para-web-fundamentos-de-html-css-APIs-na-pratica/pokedex/assets/js/main.js
+++ 2-Javascript-para-web-fundamentos-de-html-css-APIs-na-pratica/pokedex/assets/js/main.js
@@ -1,12 +1,46 @@
 //Fetch  API - Biblioteca
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-//Fazer requisição
-fetch(url).then(function(response){
-  console.log(response)
-})
+//Fazer requisição - Promise OPÇÃO 2 Arrow Function - Para reduzir o código.
+fetch(url)
+  //Converter Body ReadableStream para Json
+  .then((response) => response.json())
+  //Retorno do Json
+  .then((jsonBody) =>   console.log(jsonBody))
+  //Caso haja algum erro na requisição
+  .catch((error) => console.log(error))
+  //Informar que  a requisição foi concluída independe de erro ou não
+  .finally(() => console.log("Requisição concluída."));
+
+//-----------------------------------------------------------------------------------------------------------------------
+//Fazer requisição - Promise OPÇÃO 1 - Declarativa convencional -Callback
+
+// fetch(url)
+//Converter Body ReadableStream para Json
+// .then(function (response) {
+//   return response.json();
+// })
+//-----------------------------------------------------------------------------------------------------------------------
+
+//Retorno do Json
+// .then(function (jsonBody) {
+//   console.log(jsonBody);
+// })
+//-----------------------------------------------------------------------------------------------------------------------
+
+//Caso haja algum erro na requisição
+// .catch(function (error) {
+//   console.log(error);
+// })
+//-----------------------------------------------------------------------------------------------------------------------
+
+//Informar que  a requisição foi concluída independe de erro ou não
+// .finally(function () {
+//   console.log("Requisição concluída.");
+// });
+//-------------------------------------------------------------------------------------------------
 
 // function convertPokemonToLi(pokemon) {
 //   return `
@@ -16,7 +50,7 @@ fetch(url).then(function(response){
 
 //           <div class="detail">
 //             <ol class="types">
-//               ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')} 
+//               ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
 //             </ol>
 //             <img src="${pokemon.photo}"
 //               alt="${pokemon.name}"
@@ -34,7 +68,6 @@ fetch(url).then(function(response){
 // })
 
 //   const listItems = [];
-
 
 //   for (let i = 0; i < pokemons.length; i++) {
 //     const pokemon = pokemons[i];
