9749406ac84dac4dc7cca599a33e92caf79d70ad
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-17 17:52:52 -0300

    Manipulando a lista HTML com JS

--- 2-Javascript-para-web-fundamentos-de-html-css-APIs-na-pratica/pokedex/assets/js/main.js
+++ 2-Javascript-para-web-fundamentos-de-html-css-APIs-na-pratica/pokedex/assets/js/main.js
@@ -1,21 +1,59 @@
-//Fetch  API - Biblioteca
+//  Transformando a lista de Pokémon em uma lista de HTML - PASSO 1
+function convertPokemonToHtml(pokemon) {
+  return `       
+    <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+
+          <div class="detail">
+            <ol class="types">
+              <li class="type">grass</li>
+              <li class="type">poison</li>
+            </ol>
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+              alt="Imagem do pokemom ${pokemon.name}"
+            />
+          </div>
+        </li>
+        `;
+}
+//Manipular  o LI do HTML  com JS - PASSO 4
+const pokemonListHtmlToLi = document.getElementById("pokemonListHtml");
+
+//Fetch  API - Biblioteca - PASSO 3
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-//Fazer requisição - Promise OPÇÃO 2 Arrow Function - Para reduzir o código.
+//Fazer requisição - Promise OPÇÃO 2 Arrow Function - Para reduzir o código PASSO 4
 fetch(url)
   //Converter Body ReadableStream para Json
   .then((response) => response.json())
-  //Retorno do Json
-  .then((jsonBody) =>   console.log(jsonBody))
+
+  //Retorno do Json, pegar o resultado  da lista  do Json
+  .then((jsonBody) => jsonBody.results)
+
+  //Resulvado da lista de pokemon
+  .then((pokemonList) => {
+
+    // Chamando a Lista de pokemon ToHtml com FOR - PASSSO 5
+    for (let i = 0; i < pokemonList.length; i++) {
+      const pokemon = pokemonList[i];
+
+      //console.log(convertPokemonToHtml(pokemon))
+      // Adicionando um item - PASSO 6
+      pokemonListHtmlToLi.innerHTML += convertPokemonToHtml(pokemon);
+    }
+  })
+  /*.then((jsonBody) => console.log(jsonBody))*/
   //Caso haja algum erro na requisição
   .catch((error) => console.log(error))
   //Informar que  a requisição foi concluída independe de erro ou não
   .finally(() => console.log("Requisição concluída."));
 
 //-----------------------------------------------------------------------------------------------------------------------
-//Fazer requisição - Promise OPÇÃO 1 - Declarativa convencional -Callback
+//Fazer requisição - Promise OPÇÃO 1 - Declarativa convencional -Callback PASSO 4
 
 // fetch(url)
 //Converter Body ReadableStream para Json
@@ -42,25 +80,8 @@ fetch(url)
 // });
 //-------------------------------------------------------------------------------------------------
 
-// function convertPokemonToLi(pokemon) {
-//   return `
-//             <li class="pokemon">
-//           <span class="number">#0${pokemon.number}</span>
-//           <span class="name">${pokemon.name}</span>
 
-//           <div class="detail">
-//             <ol class="types">
-//               ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-//             </ol>
-//             <img src="${pokemon.photo}"
-//               alt="${pokemon.name}"
-//             />
-//           </div>
-//         </li>
-//     `;
-// }
 
-// const pokemonList = document.getElementById("pokemonList");
 
 // pokeApi.getPokemons().then((pokemons  = []) => {
 //     const newHtml = pokemons.map(convertPokemonToLi).join('')
