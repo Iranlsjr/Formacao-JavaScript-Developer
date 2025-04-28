7edd98298d140a48e9a2631ebe5c4f1368af184e
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-28 16:21:44 -0300

    Function

---
+++ 3-dominando-conceitos-avancados-em-javascript/funcoes.js
@@ -0,0 +1,18 @@
+function falarMeuNome() {
+  console.log("Meu nome é Iran");
+}
+
+function falarNomeCompleto(falarMeuNome) {
+  falarMeuNome();
+  console.log("Lourenço");
+}
+
+falarNomeCompleto(falarMeuNome);
+
+function nomeDaFuncao(){
+
+}
+
+const nomeDeOutraFuncao = function(){
+    
+}
